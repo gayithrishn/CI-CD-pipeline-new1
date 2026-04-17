9800a2843d7ecb4d53224752c5b9340ce072a667
gayithri <sonugowda6712gmail.com>
2026-04-17 09:45:22 +0530

    first commit

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,16 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("jenkins is a CI/CD tool");
+
+        System.out.println("poll scm is a feature in jenkins , it wil check the git repo every five minutes");
+    }
+}
